39e2eb85d722c35d659a7a57c59ce7d385c17080
Greg Turner <31353577+Greg-Turner@users.noreply.github.com>
2017-10-22 07:52:35 -0500

    added comments

--- coinsToCash.js
+++ coinsToCash.js
@@ -1,5 +1,6 @@
 // create an object with properties of the quantities of various coins
 const piggyBank = {"pennies": 342,"nickels": 83,"dimes": 21,"quarters": 15}
 
+//formular to calculate the total value - convert everything to pennies then convert total to dollars
 let dollarAmount = (piggyBank["pennies"]+(piggyBank["nickels"]*5)+(piggyBank["dimes"]*10)+(piggyBank["quarters"]*25))/100
 console.log(dollarAmount)
